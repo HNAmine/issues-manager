2501a2f730b1aaba7d64f5af945581f495e8ef10
AmineHN <hananeamine70@gmail.com>
2018-10-13 11:09:54 +0100

    little change

--- src/pages/user-login/user-login.ts
+++ src/pages/user-login/user-login.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 import { Dashboard } from '../dashboard/dashboard';
 import { UserSignup } from '../user-signup/user-signup';
 import { UserForgotpassword } from '../user-forgotpassword/user-forgotpassword';
 
-@IonicPage()
 @Component({
   selector: 'page-user-login',
   templateUrl: 'user-login.html',
--- src/pages/user-signup/user-signup.ts
+++ src/pages/user-signup/user-signup.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 import { Dashboard } from '../dashboard/dashboard';
 import { UserLogin } from '../user-login/user-login';
 import { UserForgotpassword } from '../user-forgotpassword/user-forgotpassword';
 
-@IonicPage()
 @Component({
   selector: 'page-user-signup',
   templateUrl: 'user-signup.html',

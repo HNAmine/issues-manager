539502f7d38e5734a806dcf4044833ba29ca2d17
Amine HANANE <amine.hanane@cgi.com>
2018-11-12 17:47:04 +0100

    welcome page

---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+
+@Component({
+  selector: "Wommittee",
+  templateUrl: "welcome.html"
+})
+export class WelcomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+
+}
